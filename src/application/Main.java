6094179dd1a432a8c190d95929557cd38cef3ba8
higorasilverio <higorasilverio@outlook.com>
2020-04-12 19:45:34 -0300

    Triangules areas

---
+++ src/application/Main.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangule;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter the measures of triangle X: ");
+		double a = scanner.nextDouble();
+		double b = scanner.nextDouble();
+		double c = scanner.nextDouble();
+		Triangule x = new Triangule(a, b, c);
+		System.out.println("Enter the measures of triangle Y: ");
+		a = scanner.nextDouble();
+		b = scanner.nextDouble();
+		c = scanner.nextDouble();
+		Triangule y = new Triangule(a, b, c);
+		System.out.printf("Triangle X area: %.4f%n", x.area());
+		System.out.printf("Triangle Y area: %.4f%n", y.area());
+		x.largerArea(x.area(), y.area());
+		
+		scanner.close();		
+	}
+
+}
---
+++ src/entities/Triangule.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Triangule {
+	
+	private double a;
+	private double b;
+	private double c;
+	private double area;
+	
+	
+	public Triangule(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+	
+	public double area() {
+		double p = (this.a + this.b + this.c) / 2;
+		this.area =  Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return area;
+	}	
+
+	public void largerArea(double areaX, double areaY) {
+		if (areaX > areaY) System.out.println("Larger area: X");
+		else if (areaX < areaY) System.out.println("Larger area: Y");
+		else System.out.println("The areas are equal");
+	}
+	
+}
